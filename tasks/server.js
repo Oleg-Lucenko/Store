f3ddd16934ae46163a6450e9bcd8b11f6085b6b1
Oleg-Lucenko <lucenko5577@gmail.com>
2025-04-04 20:00:48 +0300

    initial

--- tasks/server.js
+++ tasks/server.js
@@ -1,15 +1,15 @@
-import mysql from 'mysql2';
+// import mysql from 'mysql2';
 import http from "http";
 import dotenv from "dotenv";
 dotenv.config();
 
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'river',
-    database: 'products',
-    password: '4782D3e4'
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'river',
+//     database: 'products',
+//     password: '4782D3e4'
+// });
 
 
 
@@ -27,36 +27,36 @@ const server =  http.createServer(function(request, response){
 
     response.writeHead(200, headers);
 
+    response.end('Hello');
+
+    // function getProducts(category) {
+
+    //     return new Promise((resolve, reject) => {
+
+    //         connection.query(`select * from ${category}`, function(err, results) {
     
-
-    function getProducts(category) {
-
-        return new Promise((resolve, reject) => {
-
-            connection.query(`select * from ${category}`, function(err, results) {
-    
-                if(err) console.log(err);    
+    //             if(err) console.log(err);    
                 
-                resolve(results);
+    //             resolve(results);
                 
-            });
+    //         });
 
-        });
+    //     });
 
-    };
+    // };
 
 
-    if (request.url === '/smartphones') {
+    // if (request.url === '/smartphones') {
 
-        getProducts('Smartphones').then(products => response.end(JSON.stringify(products))); 
+    //     getProducts('Smartphones').then(products => response.end(JSON.stringify(products))); 
 
-    } else if (request.url === '/laptops') {
+    // } else if (request.url === '/laptops') {
 
-        getProducts('Laptops').then(products => response.end(JSON.stringify(products)));
+    //     getProducts('Laptops').then(products => response.end(JSON.stringify(products)));
 
-    } else if (request.url === '/headphones') {
-        getProducts('Headphones').then(products => response.end(JSON.stringify(products)));
-    };
+    // } else if (request.url === '/headphones') {
+    //     getProducts('Headphones').then(products => response.end(JSON.stringify(products)));
+    // };
        
 
 });
