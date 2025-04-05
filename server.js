24868ea319a7b29c6163076a87c8670d62ccdaca
Oleg-Lucenko <lucenko5577@gmail.com>
2025-04-05 19:36:20 +0300

    initial

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'river',
-    database: 'products',
-    password: '4782D3e4'
-});
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'river',
+//     database: 'products',
+//     password: '4782D3e4'
+// });
 
 
 
