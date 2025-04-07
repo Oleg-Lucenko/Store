5bdf7e644f09d867f56126d3fc028b214beeb1e7
Oleg-Lucenko <lucenko5577@gmail.com>
2025-04-08 00:32:31 +0300

    initial

--- server.js
+++ server.js
@@ -1,20 +1,53 @@
 import mysql from 'mysql2';
-import sequelize from 'sequelize';
+import Sequelize from 'sequelize';
 import http from "http";
 import dotenv from "dotenv";
 dotenv.config();
 
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'river',
-    database: 'products',
-    password: '4782D3e4',
-    port: 3306
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'river',
+//     database: 'products',
+//     password: '4782D3e4',
+//     port: 3306
+// });
+
+const sequelize = new Sequelize(
+
+    'products',
+    'river',
+    '4782D3e4',
+    {
+        host: 'localhost',
+        dialect: 'mysql'
+    }
+);
+
+
+sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+ }).catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+ });
+
+
+// const smartphone = sequelize.define( 'smartphones', {
+
+//     name: {
+//         type: Sequelize.STRING,
+//         get() {
+//             const content = this.getDataValue('name');
+//         } 
+//     }
+
+// })
+// console.log(smartphone.name.type);
+// console.log(smartphone);
+
+const allSmartphones = await sequelize.query('SELECT * FROM `smartphones`', {
+  type: Sequelize.SELECT,
 });
-
-
-
 
 
 
@@ -51,15 +84,15 @@ const server =  http.createServer(function(request, response){
 
 
 
-            connection.query(`select * from smartphones`, function(err, results) {
+            // connection.query(`select * from smartphones`, function(err, results) {
     
-                if(err) console.log(err);    
+            //     if(err) console.log(err);    
                 
-                response.end(JSON.stringify(results));
                 
-            });
+            // });
+            response.end(JSON.stringify(allSmartphones));
 
-
+            
 
     // if (request.url === '/smartphones') {
 
